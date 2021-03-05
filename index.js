728d6a79dc4076331aa1b80e095aad4b3a8558e1
truongthanhnguyen <ngtruongthanh98@gmail.com>
2021-03-05 20:19:18 +0700

    Enabling user to add new ToDo items to the list

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (request, response) => {
     return response.send('Hello, world!')
 })
@@ -21,9 +24,7 @@ app.get('/todos', (request, response) => {
             return response.json({todos: todos})
         } else {
             return response.json({todos: todos.filter(t => {return t.complete === false})})
-        }       
-
-        
+        }          
     })
 })
 
@@ -53,6 +54,31 @@ app.put('/todos/:id/complete', (request, response) => {
         }
 
         todos[todoIndex].complete = true;
+
+        fs.writeFile('./store/todos.json', JSON.stringify(todos), () => {
+            return response.json({'status': 'ok'})
+        })
+    })
+})
+
+app.post('/todo', (request, response) => {
+    if(!request.body.name){
+        return response.status(400).send('Missing name');
+    }
+
+    fs.readFile('./store/todos.json','utf-8', (err, data) => {
+        if(err){
+            return response.status(500),send('Sorry, something went wrong.')
+        } 
+        
+        const todos = JSON.parse(data)
+        const maxId = Math.max.apply(Math, todos.map(t => {return t.id}))
+
+        todos.push({
+            id: maxId + 1,
+            complete: false,
+            name: request.body.name
+        })
 
         fs.writeFile('./store/todos.json', JSON.stringify(todos), () => {
             return response.json({'status': 'ok'})
